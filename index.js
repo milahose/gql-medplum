0b766f99f1446153b87f4813217d9f39013bb11b
Mila Hose <mila.hose@gmail.com>
2023-05-24 09:24:18 -0700

    init

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const exec = require('child_process').exec;
 
 const args = "-o schema.json https://graphiql.medplum.com/schema/schema-v1.json";
 
-// exec(`curl ${args}`, (error, stdout, stderr) => {
-//   console.log('stdout: ' + stdout);
-//   console.log('stderr: ' + stderr);
-//   if (error !== null) {
-//     console.log('exec error: ' + error);
-//   }
-// });
+exec(`curl ${args}`, (error, stdout, stderr) => {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
 
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
-const {
-  buildClientSchema,
-  getIntrospectionQuery,
-  printSchema,
-} = require('graphql/utilities');
-const data = require('./schema.json')
+// const fs = require('fs');
+// const path = require('path');
+// const fetch = require('node-fetch');
+// const {
+//   buildClientSchema,
+//   getIntrospectionQuery,
+//   printSchema,
+// } = require('graphql/utilities');
+// const data = require('./schema.json')
 
 // fetch('https://graphiql.medplum.com/schema/schema-v1.json', {
 //   method: 'POST',
@@ -40,6 +40,6 @@ const data = require('./schema.json')
   //     clientSchema,
   //   ),
   // );
-  buildClientSchema(JSON.stringify(data))
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+  // buildClientSchema(JSON.stringify(data))
+  //   .then(result => console.log(result))
+  //   .catch(err => console.log(err))
